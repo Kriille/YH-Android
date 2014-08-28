0ad8b6710d3ce3c99a10b54f69b22ac0cdcfa8f8
Kriille <kristoffersvensson@live.com>
2014-08-28 13:41:36 +0200

    Hello World
    Labb

--- HelloWorld/app/src/main/java/com/example/kristoffer/helloworld/MyActivity.java
+++ HelloWorld/app/src/main/java/com/example/kristoffer/helloworld/MyActivity.java
@@ -1,9 +1,16 @@
 package com.example.kristoffer.helloworld;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.AlarmClock;
+import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 
 
 public class MyActivity extends Activity {
@@ -32,5 +39,37 @@ public class MyActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void invokeWebBrowser(View view) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("http://www.google.com"));
+        this.startActivity(intent);
+    }
+
+    public void CameraOpen(View view) {
+        Intent i = new Intent (MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(i, 0);
+    }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        if (resultCode == RESULT_OK){
+            ImageView iv= (ImageView)findViewById(R.id.imageView);
+            System.out.println();
+            Bitmap pic = (Bitmap) data.getExtras().get("data");
+            iv.setImageBitmap(pic);
+        }
+    }
+
+    public void PhoneCall(View view) {
+        Intent intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + "0734164257"));
+        startActivity(intent);
+    }
+    public void SetAlarm(View view) {
+        Intent intent = new Intent(AlarmClock.ACTION_SET_ALARM)
+                .putExtra(AlarmClock.EXTRA_HOUR, 9)
+                .putExtra(AlarmClock.EXTRA_MINUTES, 37);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 }
